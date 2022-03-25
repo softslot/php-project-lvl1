9b8801bd261b40b0ed2be9db33bec7759206d160
Konstantin Korolenko <sawnet@mail.ru>
2022-03-25 18:19:21 +0300

    Add src/Engine.php

---
+++ src/Engine.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function getPlayerName()
+{
+    return prompt('May I have your name?');
+}
+
+function getPlayerAnswer($question)
+{
+    return prompt("Question: {$question}");
+}
+
+function getNumberRounds()
+{
+    return 3;
+}
+
+function printWelcomeMsg()
+{
+    line('Welcome to the Brain Games!');
+}
+
+function printGreetingMsg($name)
+{
+    line("Hello, %s!", $name);
+}
+
+function printPlayerAnswerMsg($playerAnswer)
+{
+    line("Your answer: %s", $playerAnswer);
+}
+
+function printWrongAnswerMsg($playerAnswer, $correctAnswer, $name) {
+    line("'%s' is wrong answer ;(. Correct answer was '%s'.", $playerAnswer, $correctAnswer);
+    line("Let's try again, %s!", $name);
+}
+
+function printCorrectMsg()
+{
+    line("Correct!");
+}
+
+function printCongratulationMsg($name)
+{
+    line("Congratulations, %s!", $name);
+}
