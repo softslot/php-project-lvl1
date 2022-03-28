34e6b18709d959871a9bdea9691a9d61a3d073ac
Konstantin Korolenko <sawnet@mail.ru>
2022-03-28 16:48:19 +0300

    Add gameplay() function in src/Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -7,6 +7,28 @@ use function cli\prompt;
 
 define("NUMBER_ROUNDS", 3);
 
+function gameplay(string $title, callable $game)
+{
+    line('Welcome to the Brain Games!');
+    $name = getPlayerName();
+    line("Hello, %s!", $name);
+    line($title);
+
+    for ($round = 1; $round <= NUMBER_ROUNDS; $round += 1) {
+        [$question, $correctAnswer] = $game();
+        $playerAnswer = getPlayerAnswer($question);
+        line("Your answer: %s", $playerAnswer);
+
+        if (strval($playerAnswer) !== strval($correctAnswer)) {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $playerAnswer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            return;
+        }
+        line("Correct!");
+    }
+    line("Congratulations, %s!", $name);
+}
+
 function getPlayerName()
 {
     return prompt('May I have your name?');
